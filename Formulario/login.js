aed1264458729e5642d81654cfb7ec077b774df7
AlfredoVzla <jvalenzuela0302@gmail.com>
2024-03-03 23:23:47 -0700

    ADD fetch with server

--- Formulario/login.js
+++ Formulario/login.js
@@ -1,26 +1,38 @@
-export const login = (correo, password) => {
-    return new Promise((resolve, reject) => {
-      let endpoint = `10.2.60.117:3000/usuarios/`;
-      fetch(endpoint, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-        body: JSON.stringify({
-          correo: correo, 
-          password: password, 
-        }) 
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('registro-form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nombreUsuario = document.getElementById('correo').value;
+    const contrasena = document.getElementById('pwd').value;
+
+    console.log()
+
+    fetch('http://localhost:3000/usuarios/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        correo:nombreUsuario,
+        password: contrasena,
       })
-        .then(e => {
-          if (e.ok) {
-            resolve(e);
-          } else {
-            reject(e);
-          }
-        })
-        .catch(e => console.log("error::", e));
-    });
-  };
-  
+      
+    }).then(response => {
+      return response.json();
+    })
+      .then(data => {
+        if (data.mensaje.includes('fail')) {
+          alert('No se encontró usuario con esas credenciales');
+        } else {
+            
+          document.cookie = `token=${data.body}; path=/`;
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        alert('Error al iniciar sesión. Verifica tus credenciales.');
+      });
+  });
+});
